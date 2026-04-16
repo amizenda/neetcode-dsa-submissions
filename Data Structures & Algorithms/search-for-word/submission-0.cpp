711fa23d5c360706606360a6f541463d0a4782df
Zenda <127393937+amizenda@users.noreply.github.com>
2026-04-17 02:01:03 +0700

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int rows = board.size();
+        int cols = board[0].size();
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (backtrack(board, word, r, c, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    bool backtrack(vector<vector<char>>& board, string& word, int r, int c, int index) {
+        if (index == word.length()) return true;
+
+        if (r < 0 || r >= board.size() || 
+            c < 0 || c >= board[0].size() || 
+            board[r][c] != word[index]) {
+            return false;
+        }
+
+        char temp = board[r][c];
+        board[r][c] = '#'; 
+
+        bool found = backtrack(board, word, r + 1, c, index + 1) ||
+                     backtrack(board, word, r - 1, c, index + 1) ||
+                     backtrack(board, word, r, c + 1, index + 1) ||
+                     backtrack(board, word, r, c - 1, index + 1);
+
+        board[r][c] = temp;
+
+        return found;
+    }
+};
