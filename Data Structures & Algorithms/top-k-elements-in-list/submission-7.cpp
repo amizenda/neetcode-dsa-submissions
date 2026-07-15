259e5fdbad7bbb05d0251af5d01f49a6081847d6
Zenda <127393937+amizenda@users.noreply.github.com>
2026-07-15 18:21:19 +0700

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int, int> freq;
+
+        for(auto x : nums) {
+            freq[x]++;
+        }
+
+        vector<pair<int, int>> topFreq;
+        for(auto [key, val] : freq) {
+            topFreq.push_back({val, key});
+        }
+
+        sort(topFreq.rbegin(), topFreq.rend());
+
+        vector<int> result;
+
+        for(int i = 0; i < k; i++) {
+            result.push_back(topFreq[i].second);
+        }
+
+        return result;
+    }
+};
