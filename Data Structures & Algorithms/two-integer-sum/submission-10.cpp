0d5c5d32062846132632c9c85ae5e96d30c0ca1f
Zenda <127393937+amizenda@users.noreply.github.com>
2026-07-16 02:01:36 +0700

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int implement = target -  nums[i];
+
+            if(m.find(implement) != m.end()) {
+                return {m[implement], i};
+            }
+
+            m[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
