94ca84a3e8b92aebec3e3733422b813a7496853b
Zenda <127393937+amizenda@users.noreply.github.com>
2026-07-16 02:11:12 +0700

    Add: string-encode-and-decode - submission-10

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-10.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encodedString;
+        for(int i = 0; i < strs.size(); i++) {
+            encodedString += to_string(strs[i].size()) + '#' + strs[i];
+        }
+
+        return encodedString;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> strs;
+        int i = 0;
+        while(i < s.size()) {
+            int j = i;
+
+            while(s[j] != '#') {
+                j++;
+            }
+
+            int l = stoi(s.substr(i, j - i));
+            strs.push_back(s.substr(j + 1, l));
+
+            i = j + l + 1;
+        }
+
+        return strs;
+    }
+};
