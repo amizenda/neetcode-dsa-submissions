f84b6aa6ca2ac47d66df492c96707e1c1b607bf1
Zenda <127393937+amizenda@users.noreply.github.com>
2026-07-16 18:26:10 +0700

    Add: longest-consecutive-sequence - submission-11

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-11.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int maxSeq = 1, lSeq = 1, i = 1;
+
+        if(nums.empty()) return 0;
+
+        sort(nums.begin(), nums.end());
+
+        while(i < nums.size()) {
+            if(nums[i] != nums[i - 1]){
+                if(nums[i] - nums[i - 1] == 1) {
+                    lSeq++;
+                    maxSeq = max(maxSeq, lSeq);
+                } else {
+                    lSeq = 1;
+                }
+            }
+
+            i++;
+        }
+
+        return maxSeq;
+    }
+};
