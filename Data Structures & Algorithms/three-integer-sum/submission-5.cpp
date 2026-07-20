ba23313ccd122bcaf67b0ec32f4c732c9947528a
Zenda <127393937+amizenda@users.noreply.github.com>
2026-07-20 23:38:24 +0700

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> groups;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < nums.size() - 2; i++) {
+            int left = i + 1;
+            int right = nums.size() - 1;
+            int target = nums[i] * (-1);
+
+            while(left < right) {
+                int sum = nums[left] + nums[right];
+
+                if(sum == target) groups.insert({nums[i], nums[left], nums[right]});
+
+                if(sum < target) left++;
+                else right--;
+            }
+        }
+
+        vector<vector<int>> result;
+
+        for(auto x : groups) {
+            result.push_back(x);
+        }
+        
+        return result;
+    }
+};
