ae5c5ca28e5eef0db3b601cbab08d3b68483e6ef
Zenda <127393937+amizenda@users.noreply.github.com>
2026-07-20 23:53:57 +0700

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int left = 0;
+        int right = heights.size() - 1;
+        int maxA = 0;
+
+        while(left < right) {
+            int h = min(heights[left], heights[right]);
+            int w = right - left;
+            maxA = max(maxA, h * w);
+
+            if(heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxA;
+    }
+};
