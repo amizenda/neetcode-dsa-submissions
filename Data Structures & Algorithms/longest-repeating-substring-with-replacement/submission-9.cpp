404c8b1540166c2f735b4df579e77f762334fdc6
Zenda <127393937+amizenda@users.noreply.github.com>
2026-08-05 23:18:02 +0700

    Add: longest-repeating-substring-with-replacement - submission-9

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-9.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int> counts;
+        int res = 0;
+
+        int left = 0, maxF = 0;
+
+        for(int right = 0; right < s.size(); right++) {
+            counts[s[right]]++;
+            maxF = max(maxF, counts[s[right]]);
+
+            while((right - left + 1) - maxF > k) {
+                counts[s[left]]--;
+                left++;
+            }
+
+            res = max(res, right - left + 1);
+        }
+
+        return res;
+    }
+};
