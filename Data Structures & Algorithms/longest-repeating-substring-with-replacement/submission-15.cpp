3bfbc739810569a3a878b377284cd26cf9f050eb
Zenda <127393937+amizenda@users.noreply.github.com>
2026-08-10 23:00:49 +0700

    Add: longest-repeating-substring-with-replacement - submission-15

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-15.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int res = 0;
+        int left = 0;
+        int maxF = 0;
+        unordered_map<char, int> counts;
+
+        for(int right = 0; right < s.size(); right++) {
+            counts[s[right]]++;
+            maxF = max(maxF, counts[s[right]]);
+
+            if((right - left + 1) - maxF > k) {
+                counts[s[left]]--;
+                left++;
+            }
+
+            res = max(res, right - left + 1);
+        }
+
+        return res;
+    }
+};
