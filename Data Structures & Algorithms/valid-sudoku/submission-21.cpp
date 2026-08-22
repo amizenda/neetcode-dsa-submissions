cd4a0007036d6c26b857cb1f78fa08c8b4ab9df8
Zenda <127393937+amizenda@users.noreply.github.com>
2026-08-22 13:09:18 +0700

    Add: valid-sudoku - submission-21

---
+++ Data Structures & Algorithms/valid-sudoku/submission-21.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int cols[9][10] = {false};
+        int rows[9][10] = {false};
+        int blocks[9][10] = {false};
+
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] == '.') continue;
+
+                int num = board[i][j] - '0';
+                int k = (i / 3) * 3 + (j / 3);
+
+                if(cols[i][num] || rows[j][num] || blocks[k][num]) return false;
+
+                cols[i][num] = rows[j][num] = blocks[k][num] = true;
+            }
+        }
+
+        return true;
+    }
+};
