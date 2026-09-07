92ab3236183f7d7a2e87c8a66accd727acd08361
Zenda <127393937+amizenda@users.noreply.github.com>
2026-09-07 10:28:56 +0700

    Add: permutation-string - submission-55

---
+++ Data Structures & Algorithms/permutation-string/submission-55.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length()) return false;
+
+        vector<int> count1(26, 0);
+        vector<int> count2(26, 0);
+
+        for(char c : s1) {
+            count1[c - 'a']++;
+        }
+
+        int left = 0;
+        for(int right = 0; right < s2.size(); right++) {
+            count2[s2[right] - 'a']++;
+
+            if(right - left + 1 > s1.size()) {
+                count2[s2[left] - 'a']--;
+                left++;
+            }
+
+            if(count1 == count2) return true;
+        }
+
+        return false;
+    }
+};
