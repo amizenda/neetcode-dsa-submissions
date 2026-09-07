727b1e5119bab69c2a68c5bdd3f380605ae8e74a
Zenda <127393937+amizenda@users.noreply.github.com>
2026-09-07 10:09:14 +0700

    Add: trapping-rain-water - submission-27

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-27.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int maxPos = -1;
+        int maxHeight = 0;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(height[i] > maxHeight) {
+                maxHeight = height[i];
+                maxPos = i;
+            }
+        }
+
+        int leftMax = height[0];
+        for(int i = 1; i < maxPos; i++) {
+            if(height[i] > leftMax) {
+                leftMax = height[i];
+            } else {
+                sum += leftMax - height[i];
+            }
+        }
+
+        int rightMax = height[n - 1];
+        for(int i = n - 2; i > maxPos; i--) {
+            if(height[i] > rightMax) {
+                rightMax = height[i];
+            } else {
+                sum += rightMax - height[i];
+            }
+        }
+
+        return sum;
+    }
+};
